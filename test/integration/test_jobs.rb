7b1d982db438a29a0802389fc5b41635916bddb4
Lewis Marshall <lewis@lmars.net>
2014-03-05 13:54:23 +0000

    Test against a multi host grid
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- test/integration/test_jobs.rb
+++ test/integration/test_jobs.rb
@@ -3,18 +3,24 @@ require "test_helper"
 class TestJobs < GridIntegrationTest
   def test_all_jobs_are_returned
     jobs = @grid.jobs
-    assert_equal 2, jobs.size
-    assert_match /^flynn-etcd/, jobs[0].id
-    assert_match /^flynn-discoverd/, jobs[1].id
+    assert_equal 6, jobs.size
+
+    @grid.hosts.each do |host|
+      jobs = host.jobs
+      assert_match /^flynn-etcd/, jobs[0].id
+      assert_match /^flynn-discoverd/, jobs[1].id
+    end
   end
 
   def test_schedule_job
     jobs = @grid.jobs
-    assert_equal 2, jobs.size
+    assert_equal 6, jobs.size
 
     @grid.schedule "app"
     jobs = @grid.jobs
-    assert_equal 3, jobs.size
-    assert_match /^app/, jobs[2].id
+    assert_equal 7, jobs.size
+
+    first_host = @grid.hosts.first
+    assert_match /^app/, first_host.jobs[2].id
   end
 end
